d31e1380a208f773c7dd9841ee1d420d03709e4c
Olivier Paroz <github@oparoz.com>
2014-12-03 23:52:29 +0100

    Generate thumbnails of all supported media types
    The batch processor now returns base64 encoded strings of all images
    which can be generated by a preview provider.
    It also sends the correct media type so that the browser can always
    display what it receives.

--- ajax/batch.php
+++ ajax/batch.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Copyright (c) 2012 Robin Appelman <icewind@owncloud.com>
+ * Copyright (c) 2014 Olivier Paroz <owncloud@oparoz.com>
  * This file is licensed under the Affero General Public License version 3 or
  * later.
  * See the COPYING-README file.
@@ -10,14 +11,16 @@ OCP\JSON::checkAppEnabled('gallery');
 
 $square = isset($_GET['square']) ? (bool)$_GET['square'] : false;
 $scale = isset($_GET['scale']) ? $_GET['scale'] : 1;
+$token = isset($_GET['token']) ? $_GET['token'] : false;
 $images = explode(';', $_GET['image']);
 
-if (!empty($_GET['token'])) {
-	$linkItem = \OCP\Share::getShareByToken($_GET['token']);
+if ($token) {
+	$linkItem = \OCP\Share::getShareByToken($token);
 	if (!(is_array($linkItem) && isset($linkItem['uid_owner']))) {
 		exit;
 	}
 	// seems to be a valid share
+	$fileSource = $linkItem['file_source'];
 	$rootLinkItem = \OCP\Share::resolveReShare($linkItem);
 	$user = $rootLinkItem['uid_owner'];
 
@@ -26,7 +29,7 @@ if (!empty($_GET['token'])) {
 	OC_Util::tearDownFS();
 	OC_Util::setupFS($user);
 
-	$root = \OC\Files\Filesystem::getPath($linkItem['file_source']) . '/';
+	$root = \OC\Files\Filesystem::getPath($fileSource) . '/';
 	$images = array_map(function ($image) use ($root) {
 		return $root . $image;
 	}, $images);
@@ -40,32 +43,81 @@ session_write_close();
 $eventSource = new OC_EventSource();
 
 foreach ($images as $image) {
-	$height = 200 * $scale;
-	if ($square) {
-		$width = 200 * $scale;
-	} else {
-		$width = 400 * $scale;
-	}
-
-	$userView = new \OC\Files\View('/' . $user);
-
-	$preview = new \OC\Preview($user, 'files', '/' . $image, $width, $height);
-	$preview->setKeepAspect(!$square);
-
-	$fileInfo = $userView->getFileInfo('/files/' . $image);
-
-	// if the thumbnails is already cached, get it directly from the filesystem to avoid decoding and re-encoding the image
 	$imageName = substr($image, strlen($root));
-	if ($path = $preview->isCached($fileInfo->getId())) {
-		$eventSource->send('preview', array(
-			'image' => $imageName,
-			'preview' => base64_encode($userView->file_get_contents('/' . $path))
-		));
+	$userView = new \OC\Files\View('/' . $user);
+	$fileInfo = $userView->getFileInfo('/files/' . $image);
+	
+	// Prevents the script from breaking if we encounter an error
+	if (is_object($fileInfo)) {
+		$mimeType = $fileInfo->getMimeType();
+		$filePath = $fileInfo->getPath();
+		$imageId = $fileInfo->getId();
+		//$encryptedVolume = $fileInfo->isEncrypted();
+		//$mountedVolume = $fileInfo->isMounted();
+		OC_Log::write('gallery', '[Batch] File: ' . $filePath . ', Mime: ' . $mimeType, \OC_Log::DEBUG);
 	} else {
-		$eventSource->send('preview', array(
-			'image' => $imageName,
-			'preview' => (string)$preview->getPreview()
-		));
+		OC_Log::write('gallery', '[Batch] WARNING : ' . $image . ' is not a Fileinfo Object', \OC_Log::DEBUG);
+		continue;
 	}
+	
+	$preview = new \OC\Preview($user, 'files');
+	// Yet another exception for native SVG support...
+	if ($mimeType === 'image/svg+xml' && !$preview->isMimeSupported($mimeType)) {
+		// if ($encryptedVolume === true || $mountedVolume = true) {
+			// $local = $ownerView->toTmpFile($image);
+		// } else {
+			// $local = $ownerView->getLocalFile($image);
+		// }
+		// Does this work with encrypted data?
+		$previewdata = base64_encode($userView->file_get_contents('/files/' . $image));
+		$previewmime = $mimeType;
+		OC_Log::write('gallery', '[Batch] Using native SVG rendering', \OC_Log::DEBUG);
+	} else {
+		if ($square) {
+			$height = $width = 100 * $scale;
+		} else {
+			$width = 400 * $scale;
+			$height = 200 * $scale;
+		}
+
+		$preview->setFile('/' . $image);
+		$preview->setMaxX($width);
+		$preview->setMaxY($width);
+		// Crop and center for square pictures. Resized picture for large thumbnails
+		$preview->setKeepAspect(!$square);
+		// A bestfit method is missing so that square pictures don't look distorted
+		// when one dimension of the original pictures is smaller
+
+		// if the thumbnails is already cached, get it directly from the filesystem
+		// to avoid decoding and re-encoding the image
+		if ($path = $preview->isCached($imageId)) {
+			OC_Log::write('gallery', '[Batch] Using a cached preview', \OC_Log::DEBUG);
+			$previewdata = base64_encode($userView->file_get_contents('/' . $path));
+			$previewmime = $userView->getFileInfo('/' . $path)->getMimeType();
+		} else {
+			OC_Log::write('gallery', '[Batch] Generating a new preview', \OC_Log::DEBUG);
+			$previewdata = $preview->getPreview();
+			if (!$previewdata->valid()) {
+				OC_Log::write('gallery', '[Batch] ERROR! Did not get a preview', \OC_Log::DEBUG);
+				// Won't be necessary if previews send the mime icon when it's broken
+				// https://github.com/owncloud/core/pull/12546		
+				$previewdata = new \OC_Image();
+				$icon = \OC::$SERVERROOT . \OC_Helper::mimetypeIcon($mimeType);
+				$previewdata->loadFromFile($icon);
+			}
+			$previewmime = $previewdata->mimeType();
+			
+		}
+	}
+
+	OC_Log::write('gallery', '[Batch] THUMBNAIL NAME : ' . $imageName , \OC_Log::DEBUG);
+	OC_Log::write('gallery', '[Batch] THUMBNAIL MIME : ' . $previewmime , \OC_Log::DEBUG);
+	OC_Log::write('gallery', '[Batch] THUMBNAIL DATA : ' . substr((string)$previewdata, 0, 20) , \OC_Log::DEBUG);
+	
+	$eventSource->send('preview', array(
+			'image' => $imageName,
+			'mimetype' => $previewmime,
+			'preview' => (string)$previewdata
+	));
 }
 $eventSource->close();
